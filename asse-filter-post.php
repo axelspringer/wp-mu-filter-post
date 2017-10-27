82a74d36e610cd6bad2a5931bee2120ebfe1c3af
Steve Junker <steve.junker@axelspringer.de>
2017-10-27 10:38:35 +0200

    PEA-5811: > replace the cached article embed html backend urls with the frontend ones

--- asse-filter-post.php
+++ asse-filter-post.php
@@ -20,3 +20,47 @@ function replaceContentUrls($data)
     return $data;
 }
 add_filter('wp_insert_post_data', 'replaceContentUrls', 99, 1);
+
+/**
+ * Replace the backend article urls in an oembed html template with the frontend urls
+ *
+ * @see PEA-5811
+ * @see WP_Embed::shortcode()
+ *
+ * <blockquote class="wp-embedded-content">
+ *   <a href="http://192.168.33.10:41801/pmtest/ma-test-2">MA Test</a>
+ * </blockquote>
+ * <iframe src="http://192.168.33.10:41801/pmtest/ma-test-2/embed"></iframe>
+ *
+ * @param string $html
+ * @param string $url
+ * @param string $attr
+ * @param int $postId
+ * @return string
+ */
+function replaceEmbedHtmlUrl($html, $url, $attr, $postId)
+{
+    // we cannot modify the urls for the backend because it does not know the frontend
+    if ('backend' === getenv('WP_LAYER')) {
+        return $html;
+    }
+
+    // find the replaceable urls in html
+    $result = preg_match('/href="(.*?)"/', $html, $match);
+
+    if (!$result || !isset($match[1])) {
+        return $html;
+    }
+
+    $url = $match[1];
+
+    // generate the url
+    $frontendUrl = esc_url(get_permalink($postId));
+
+    // replace backend urls
+    $html = str_replace($url, $frontendUrl, $html);
+
+    return $html;
+}
+
+add_filter('embed_oembed_html', 'replaceEmbedHtmlUrl', 10, 4);
